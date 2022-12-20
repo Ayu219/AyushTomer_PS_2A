4be3f58f28534d21305f4fbb5fb01f5abe293245
Ayush <rajputayush152@gmail.com>
2022-12-20 14:16:01 +0530

    New Question

---
+++ test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int 
+    ;
+    a=(int *)calloc(n,sizeof(n));
+    for(i=0;i<n;i++){
+            scanf("%d",a+i);
+      }
+      
+      for(i=0;i<n;i++){
+              printf("%d",*(a+i));
+        }
+        for(i=1;i<=n;i++){
+           for(j=1;j<=i;j++){
+                printf("%d",j);
+          }
+        printf("\n");
+        }
+      
+      
+    
+    
+return 0;
+}
